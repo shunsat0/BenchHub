793b9a32e6fcb1dad6090082a1e32a8e3fcf19fc
shun <ellegardenno8@gmail.com>
2023-12-29 00:38:22 +0900

    Initial Commit

---
+++ BenchHub/BenchHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenchHubApp.swift
+//  BenchHub
+//
+//  Created by Shun Sato on 2023/12/29.
+//
+
+import SwiftUI
+
+@main
+struct BenchHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
