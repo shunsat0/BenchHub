dd041a5a284ba241a3106c5f93b525c64bdc6eef
shun <ellegardenno8@gmail.com>
2024-02-05 00:28:35 +0900

    update : サンプルのモデルを作成

--- BenchHub/MapModel.swift
+++ BenchHub/MapModel.swift
@@ -17,11 +17,14 @@ struct MapModel:Identifiable{
     }
     let name: String
     let ImageName: String
+    let description: String
 }
 
 struct MockData {
+    static let sample = MapModel(latitude: 35.561282, longitude: 139.711039, name: "西蒲田公園",ImageName: "bench",description: "ホームレースがよく日光を浴びています。夕方になると、キッズたちが元気よく遊んでいます。")
+    
     static let mapData = [
-        MapModel(latitude: 35.561282, longitude: 139.711039, name: "西蒲田公園",ImageName: ""),
-        MapModel(latitude: 35.562362, longitude: 139.715177, name: "蒲田駅西口", ImageName: "")
+        MapModel(latitude: 35.561282, longitude: 139.711039, name: "西蒲田公園",ImageName: "bench",description: "ホームレースがよく日光を浴びています。夕方になると、キッズたちが元気よく遊んでいます。"),
+        MapModel(latitude: 35.562362, longitude: 139.715177, name: "蒲田駅西口", ImageName: "bench",description: "交番の前の広場にアルミ製のベンチがあります。足を乗せていると、おじさんに注意されます。")
     ]
 }
