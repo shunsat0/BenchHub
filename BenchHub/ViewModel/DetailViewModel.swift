7330c75fa20fe188cb063a626f4918a19c7c9f09
shun <ellegardenno8@gmail.com>
2024-02-07 01:19:14 +0900

    add : 選択した位置のデータを一時保存するためのViewModelを作成

---
+++ BenchHub/ViewModel/DetailViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewModel.swift
+//  BenchHub
+//
+//  Created by Shun Sato on 2024/02/07.
+//
+
+import Foundation
+
+final class DetailViewModel: ObservableObject {
+    
+    var selectedFramework: MapModel?
+    
+}
