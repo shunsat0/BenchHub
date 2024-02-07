5d70dd040d51cd26020380f30267892149d8e532
shun <ellegardenno8@gmail.com>
2024-02-08 00:56:53 +0900

    add : reviewのモデルを作成

--- BenchHub/Model/MapModel.swift
+++ BenchHub/Model/MapModel.swift
@@ -17,14 +17,17 @@ struct MapModel:Identifiable{
     }
     let name: String
     let ImageName: String
-    let description: String
+    let reviews: [Review]
 }
 
-//struct MockData {
-//    static let sample = MapModel(latitude: 35.561282, longitude: 139.711039, name: "西蒲田公園",ImageName: "bench",description: "ホームレースがよく日光を浴びています。夕方になると、キッズたちが元気よく遊んでいます。")
-//    
-//    static let mapData = [
-//        MapModel(latitude: 35.561282, longitude: 139.711039, name: "西蒲田公園",ImageName: "bench",description: "ホームレースがよく日光を浴びています。夕方になると、キッズたちが元気よく遊んでいます。"),
-//        MapModel(latitude: 35.562362, longitude: 139.715177, name: "蒲田駅西口", ImageName: "bench",description: "交番の前の広場にアルミ製のベンチがあります。足を乗せていると、おじさんに注意されます。")
-//    ]
-//}
+struct Review: Identifiable,Hashable {
+    let id = UUID()
+    let description: String
+    let rating: Int
+    let title: String
+}
+
+
+struct MockData {
+    static let sample = MapModel(latitude: 35.561282, longitude: 139.711039, name: "西蒲田公園",ImageName: "bench", reviews: [Review(description: "test", rating: 2,title: "タイトル"),Review(description: "test2", rating: 5,title: "タイトル2")] )
+}
