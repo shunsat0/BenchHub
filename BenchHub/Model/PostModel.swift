cfaaa76a7a6d15d0a550c63cb9d4c35c3fd8e90d
shun <ellegardenno8@gmail.com>
2024-02-13 23:23:38 +0900

    add : ポストモデルの作成

---
+++ BenchHub/Model/PostModel.swift
@@ -0,0 +1,15 @@
+//
+//  PostModel.swift
+//  BenchHub
+//
+//  Created by Shun Sato on 2024/02/11.
+//
+
+import Foundation
+
+struct PostModel {
+    let id: String
+    let evaluation: Int
+    let title: String
+    let description: String
+}
