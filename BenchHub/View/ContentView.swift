2aa12cf80f4ed896c60e80dcc181baefe5cf3de6
shun <ellegardenno8@gmail.com>
2024-02-14 19:34:54 +0900

    add : ZStackで現在地へ戻るボタンを追加

--- BenchHub/View/ContentView.swift
+++ BenchHub/View/ContentView.swift
@@ -11,37 +11,53 @@ import MapKit
 struct ContentView: View {
     @StateObject var viewModel = MapDataViewModel()
     @StateObject var detailViewModel = DetailViewModel()
-        
+    
     @State var position: MapCameraPosition = .automatic
     @State var isShowSheet: Bool = false
     
     var body: some View {
-        Map() {
-            ForEach(viewModel.mapData) { mapInfo in
-                Annotation(mapInfo.name, coordinate: mapInfo.coordinate) {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 5)
-                            .fill(.orange)
-                        Text("🪑")
-                            .padding(5)
-                    }
-                    .onTapGesture {
-                        detailViewModel.selectedFramework = mapInfo
-                        isShowSheet = true
-                    }
-                    .sheet(isPresented: $isShowSheet) {
-                        DetailView(selectedMapInfo: detailViewModel.selectedFramework!)
-                            .presentationDetents([ .medium, .large])
-                            .presentationBackground(Color.background)
+        ZStack {
+            Map() {
+                ForEach(viewModel.mapData) { mapInfo in
+                    Annotation(mapInfo.name, coordinate: mapInfo.coordinate) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(.orange)
+                            Text("🪑")
+                                .padding(5)
+                        }
+                        .onTapGesture {
+                            detailViewModel.selectedFramework = mapInfo
+                            isShowSheet = true
+                        }
+                        .sheet(isPresented: $isShowSheet) {
+                            DetailView(selectedMapInfo: detailViewModel.selectedFramework!)
+                                .presentationDetents([ .medium, .large])
+                                .presentationBackground(Color.background)
                             
+                        }
                     }
                 }
             }
-        }
-        .onAppear {
-            Task {
-                await viewModel.fetchData()
+            .onAppear {
+                Task {
+                    await viewModel.fetchData()
+                }
             }
+            
+            Button {
+                // 現在地に戻る
+                
+            }label: {
+                Image(systemName: "location.fill")
+                    .resizable()
+                    .frame(width: 35.0,height: 35.0)
+                    .padding(5)
+                    .background(Color.component)
+                    .cornerRadius(10)
+            }
+            .padding(.leading,300)
+            .padding(.top,700)
         }
     }
 }
