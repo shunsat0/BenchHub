0658cc61aa84837b1140b74625ed3fab49f33f5b
shun <ellegardenno8@gmail.com>
2024-02-18 21:19:48 +0900

    add : AppCheckの導入
    エラーが出たので対応

--- BenchHub/BenchHubApp.swift
+++ BenchHub/BenchHubApp.swift
@@ -7,27 +7,31 @@
 
 import SwiftUI
 import FirebaseCore
+import FirebaseAppCheck
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        let providerFactory = MyAppCheckProviderFactory()
+        AppCheck.setAppCheckProviderFactory(providerFactory)
+        FirebaseApp.configure()
+        
+        return true
+    }
 }
 
 
 @main
 struct YourApp: App {
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
-  var body: some Scene {
-    WindowGroup {
-      NavigationView {
-          ContentView(isShowReviewSheet: false, isPost: false)
-        //TestView()
-      }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView(isShowReviewSheet: false, isPost: false)
+                //TestView()
+            }
+        }
     }
-  }
 }
