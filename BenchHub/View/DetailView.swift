e85327b9f6c2d5480315a068bbd9171454f98217
shun <ellegardenno8@gmail.com>
2024-02-26 23:56:25 +0900

    add : 画像をタップしたときに一覧用のsheetを立ち上がるようにした

--- BenchHub/View/DetailView.swift
+++ BenchHub/View/DetailView.swift
@@ -315,9 +315,10 @@ struct ReviewAndDistanceView: View {
 
 struct ImagesView: View {
     var mapInfo: MapModel
-    
+    @State var showImageList: Bool = false
+
     var body: some View {
-        ScrollView(.horizontal,showsIndicators: false) {
+        ScrollView(.horizontal, showsIndicators: false) {
             LazyHStack(spacing: 16) {
                 ForEach(mapInfo.reviews, id: \.id) { images in
                     if let imageUrl = URL(string: images.ImageUrl) {
@@ -330,15 +331,54 @@ struct ImagesView: View {
                             ProgressView()
                         }
                         .frame(width: 250, height: 250)
+                        .onTapGesture {
+                            print("画像タップ")
+                            showImageList = true // ここをtoggleからtrueに変更
+                        }
                     } else {
                         EmptyView()
                     }
                 }
             }
         }
+        .sheet(isPresented: $showImageList) {
+            ImagesListView(mapInfo: mapInfo, showImageList: $showImageList)
+        }
     }
 }
 
+
+
+struct ImagesListView: View {
+    var mapInfo: MapModel
+    @Binding var showImageList: Bool
+    
+    var body: some View {
+        ScrollView(.horizontal,showsIndicators: false) {
+            LazyVStack(spacing: 16) {
+                ForEach(mapInfo.reviews, id: \.id) { images in
+                    if let imageUrl = URL(string: images.ImageUrl) {
+                        // imageUrlがnilでない場合に実行
+                        AsyncImage(url: imageUrl) { image in
+                            image.resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(10.0)
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 250, height: 250)
+                        .onTapGesture {
+                            print("画像タップ")
+                        }
+                    } else {
+                        EmptyView()
+                    }
+                }
+            }
+        }
+        
+    }
+}
 
 
 struct CommentView: View {
@@ -389,9 +429,9 @@ struct CommentView: View {
     }
 }
 
-//#Preview {
-//    DetailView(isShowPostSheet: false, selectedMapInfo: sample, isPostReview: .constant(false), isShowReviewSheet: .constant(false),isGoodOrBad: false)
-//}
+#Preview {
+    DetailView(isShowPostSheet: false, selectedMapInfo: sample, isPostReview: .constant(false), isShowReviewSheet: .constant(false),isGoodOrBad: false, getedData: .constant(false))
+}
 
 var sample = MapModel(latitude: 35.561282, longitude: 139.711039, name: "西蒲田公園",reviews: [Review(description: "公園のベンチは非常に快適で、座り心地が良いです。木陰に配置されており、景色を楽しみながらくつろげます。メンテナンスも行き届いており、清潔感があります。公園を訪れる人々にとって、素晴らしい休憩スポットとなっています。", evaluation: 0, ImageUrl: "https://1.bp.blogspot.com/-ezrLFVDoMhg/Xlyf7yQWzaI/AAAAAAABXrA/utIBXYJDiPYJ4hMzRXrZSHrcZ11sW2PiACNcBGAsYHQ/s1600/no_image_yoko.jpg"),Review(description: "公園のベンチは老朽化しており、座面が不安定です。背もたれもないため、長時間座っていると疲れやすく、くつろぐことができません。また、周囲にゴミや汚れが散乱しており、清潔さを欠いています。公園全体のメンテナンスが行き届いていない印象を受けます。", evaluation: 1, ImageUrl: "https://1.bp.blogspot.com/-ezrLFVDoMhg/Xlyf7yQWzaI/AAAAAAABXrA/utIBXYJDiPYJ4hMzRXrZSHrcZ11sW2PiACNcBGAsYHQ/s1600/no_image_yoko.jpg")] )
 
