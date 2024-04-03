3449215f738bf1bc54b2807dbf6480c747d26354
youten410 <techregisteremail@gmail.com>
2024-04-03 23:41:27 +0900

    update : 設定画面の変更

--- BenchHub/View/SettingView.swift
+++ BenchHub/View/SettingView.swift
@@ -6,8 +6,7 @@
 //
 
 import SwiftUI
-
-import SwiftUI
+import MapKit
 
 struct SettingMenue: Identifiable, Hashable {
     let name: String
@@ -16,68 +15,102 @@ struct SettingMenue: Identifiable, Hashable {
 }
 
 private var settings = [
-    
     SettingMenue(name: "よくある質問",icon: "questionmark.circle"),
     SettingMenue(name: "利用規約",icon: "doc.plaintext"),
     SettingMenue(name: "プライバシーポリシー",icon: "hand.raised")
 ]
 
 
-
 struct SettingView: View {
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        
-        NavigationView {
-            List {
-                
-                Section(header: Text("通知")){
+        List {
+            Section(header: Text("通知")){
+                NavigationLink(
+                    destination: EmptyView(),
+                    label: {
+                        HStack {
+                            Image(systemName: "bell")
+                                .foregroundColor(.accentColor)
+                            Text("通知設定")
+                        }
+                    }
+                    
+                )
+            }
+            Section(header: Text("その他")){
+                ForEach(settings) { setting in
                     NavigationLink(
-                        destination: ContentView(),
+                        destination:  EmptyView(),
                         label: {
                             HStack {
-                                Image(systemName: "bell")
+                                Image(systemName: setting.icon)
                                     .foregroundColor(.accentColor)
-                                Text("通知設定")
+                                Text(setting.name)
                             }
                         }
-                    
                     )
-                }
-                Section(header: Text("その他")){
-                    ForEach(settings) { setting in
-                        NavigationLink(
-                            destination: ContentView(),
-                            label: {
-                                HStack {
-                                    Image(systemName: setting.icon)
-                                        .foregroundColor(.accentColor)
-                                    Text(setting.name)
-                                }
-                            }
-                        )
-                        
-                    }
+                    
                 }
             }
-        }
-        .navigationTitle("設定")
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading){
-                Button(
-                    action: {
-                        dismiss()
-                    }, label: {
-                        Text("\(Image(systemName: "chevron.backward"))戻る")
+            Section(header: Text("口コミ")){
+                NavigationLink(
+                    destination: PostBenchInfoView(),
+                    label: {
+                        HStack {
+                            Image(systemName: "chair")
+                                .foregroundColor(.accentColor)
+                            Text("ベンチ情報を追加")
+                        }
                     }
-                ).tint(.accentColor)
+                )
+            }
+        }
+    }
+}
+
+struct PostBenchInfoView: View {
+    @State var position: MapCameraPosition = .userLocation(fallback: .automatic)
+    @State var placeName:String = ""
+    @State private var coordinate: CLLocationCoordinate2D = .init(latitude: 0.00,
+                                                                  longitude: 0.00)
+    let locationManager = CLLocationManager()
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                List{
+                    Section(header: Text("口コミ")){
+                        MapReader{proxy in
+                            Map(position: $position)
+                                .frame(width: 300,height: 200)
+                                .cornerRadius(10.0)
+                                .task {
+                                    let manager = CLLocationManager()
+                                    manager.requestWhenInUseAuthorization()
+                                }
+//                                .onTapGesture { position in
+//                                    Marker(coordinate: coordinate, label: Text("test"))
+//                                    if let selectedCoordinate = proxy.convert(position, from: .local) {
+//                                        coordinate = selectedCoordinate
+//                                    }
+//                                }
+                        }
+                    }
+                    TextField(text: $placeName, prompt: Text("場所名")) {
+                        Text("placeName")
+                    }
+                    
+                    Text("latitude: \(coordinate.latitude)")
+                    Text("longitude: \(coordinate.longitude)")
+                }
             }
         }
     }
 }
 
 #Preview {
-    SettingView()
+    //SettingView()
+    PostBenchInfoView()
 }
